30ee3482d3bf47ea7b79de9a290bf4a4ca6edd03
apereverzin <andrey.pereverzin@hotmail.com>
2020-03-09 15:12:55 +0000

    Added test

---
+++ src/test/java/uk/gov/hmcts/reform/notificationservice/ApplicationTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.notificationservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest {
+    @Test
+    void dummyTest() {
+        assertThat(2 > 1).isTrue();
+    }
+
+}
