998d34da7611c1e019829d3b024b3927fb09ed07
Donatas Martinkus <doncem@users.noreply.github.com>
2020-03-10 15:07:59 +0000

    Provision DB

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,57 @@
 provider "azurerm" {
   version = "=1.42.0"
 }
+
+module "reform-blob-router-db" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = "${var.product}-${var.component}"
+  location           = var.location_db
+  env                = var.env
+  database_name      = "notifications"
+  postgresql_user    = "notifier"
+  postgresql_version = "11"
+  sku_name           = "GP_Gen5_2"
+  sku_tier           = "GeneralPurpose"
+  common_tags        = var.common_tags
+  subscription       = var.subscription
+}
+
+data "azurerm_key_vault" "reform_scan_key_vault" {
+  name                = "reform-scan-${var.env}"
+  resource_group_name = "reform-scan-${var.env}"
+}
+
+# region DB secrets
+# names have to be in such format as library hardcodes them for migration url build
+
+resource "azurerm_key_vault_secret" "db_user" {
+  key_vault_id = data.azurerm_key_vault.reform_scan_key_vault.id
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.reform-blob-router-db.user_name
+}
+
+resource "azurerm_key_vault_secret" "db_password" {
+  key_vault_id = data.azurerm_key_vault.reform_scan_key_vault.id
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.reform-blob-router-db.postgresql_password
+}
+
+resource "azurerm_key_vault_secret" "db_host" {
+  key_vault_id = data.azurerm_key_vault.reform_scan_key_vault.id
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.reform-blob-router-db.host_name
+}
+
+resource "azurerm_key_vault_secret" "db_port" {
+  key_vault_id = data.azurerm_key_vault.reform_scan_key_vault.id
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = module.reform-blob-router-db.postgresql_listen_port
+}
+
+resource "azurerm_key_vault_secret" "db_database" {
+  key_vault_id = data.azurerm_key_vault.reform_scan_key_vault.id
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.reform-blob-router-db.postgresql_database
+}
+
+# endregion
