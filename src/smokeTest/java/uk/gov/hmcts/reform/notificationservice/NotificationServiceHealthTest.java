8738c05248f7284dada29e0501901f9fe7c453ec
Nitin Prabhu <prabhunitin61@gmail.com>
2020-03-12 12:24:31 +0000

    Added smoke tests for the pipeline to pass

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/notificationservice/NotificationServiceHealthTest.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor;
+
+import com.typesafe.config.ConfigFactory;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.hamcrest.Matchers.equalTo;
+
+@TestPropertySource("classpath:application.conf")
+class NotificationServiceHealthTest {
+
+    private static final String TEST_URL = ConfigFactory.load().getString("test-url");
+
+    @Test
+    void notification_service_is_healthy() {
+        RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(TEST_URL)
+            .get("/health")
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.OK.value())
+            .and()
+            .body("status", equalTo(Status.UP.toString()));
+    }
+}
