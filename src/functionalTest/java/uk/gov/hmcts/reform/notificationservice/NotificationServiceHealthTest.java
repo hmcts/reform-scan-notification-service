31d0d6ebee938015b57561a8962fe02f38d84c65
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2020-03-13 09:09:03 +0000

    Set flag to enable aks deployment on staging (#8)

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/notificationservice/NotificationServiceHealthTest.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.bulkscan.payment.processor;
+
+import com.typesafe.config.ConfigFactory;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
+
+import static org.hamcrest.Matchers.equalTo;
+
+class NotificationServiceHealthTest {
+
+    private static final String TEST_URL = ConfigFactory.load().getString("test-url");
+
+    @Test
+    void notification_service_is_healthy() {
+        // TODO: this test is there so that a test report can be created for smoke tests
+        // (otherwise the build fails). Remove when actual functional tests have been written.
+        RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(TEST_URL)
+            .get("/health")
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.OK.value())
+            .and()
+            .body("status", equalTo(Status.UP.toString()));
+    }
+}
