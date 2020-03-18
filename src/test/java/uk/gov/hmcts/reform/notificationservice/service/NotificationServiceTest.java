e4a4d9ab3459396461ce8283126f8e9f88d2830c
Donatas Martinkus <doncem@users.noreply.github.com>
2020-03-18 14:17:09 +0000

    Add NotificationService simple entry

---
+++ src/test/java/uk/gov/hmcts/reform/notificationservice/service/NotificationServiceTest.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.notificationservice.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.notificationservice.data.NotificationRepository;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+    @Mock
+    private NotificationRepository notificationRepository;
+
+    private NotificationService notificationService;
+
+    @BeforeEach
+    void setUp() {
+        notificationService = new NotificationService(notificationRepository);
+    }
+
+    @Test
+    void should_call_repository_for_pending_notifications_to_begin_the_process() {
+        notificationService.processPendingNotifications();
+
+        verify(notificationRepository, times(1)).findPending();
+    }
+}
