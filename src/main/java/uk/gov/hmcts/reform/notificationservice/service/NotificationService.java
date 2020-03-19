eab2dc202cab9fb774d42d17dfa8f0dd97f2544d
Donatas Martinkus <doncem@users.noreply.github.com>
2020-03-19 11:50:46 +0000

    Add happy path of error notification

--- src/main/java/uk/gov/hmcts/reform/notificationservice/service/NotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/service/NotificationService.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.notificationservice.service;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.notificationservice.clients.ErrorNotificationClient;
+import uk.gov.hmcts.reform.notificationservice.clients.ErrorNotificationRequest;
+import uk.gov.hmcts.reform.notificationservice.clients.ErrorNotificationResponse;
 import uk.gov.hmcts.reform.notificationservice.data.Notification;
 import uk.gov.hmcts.reform.notificationservice.data.NotificationRepository;
 
@@ -15,16 +19,40 @@ public class NotificationService {
     private static final Logger log = getLogger(NotificationService.class);
 
     private final NotificationRepository notificationRepository;
+    private final ErrorNotificationClient notificationClient;
 
     public NotificationService(
-        NotificationRepository notificationRepository
+        NotificationRepository notificationRepository,
+        ErrorNotificationClient notificationClient
     ) {
         this.notificationRepository = notificationRepository;
+        this.notificationClient = notificationClient;
     }
 
     public void processPendingNotifications() {
         List<Notification> notifications = notificationRepository.findPending();
 
         log.info("Notifications to process: {}", notifications.size());
+
+        notifications
+            .stream()
+            .map(notification -> Pair.of(notification.id, mapToRequest(notification)))
+            .forEach(pair -> processNotifications(pair.getKey(), pair.getValue()));
+    }
+
+    private ErrorNotificationRequest mapToRequest(Notification notification) {
+        return new ErrorNotificationRequest(
+            notification.zipFileName,
+            notification.poBox,
+            notification.errorCode.name(),
+            notification.errorDescription,
+            String.valueOf(notification.id)
+        );
+    }
+
+    private void processNotifications(long id, ErrorNotificationRequest request) {
+        ErrorNotificationResponse response = notificationClient.notify(request);
+
+        notificationRepository.markAsSent(id, response.getNotificationId());
     }
 }
--- src/test/java/uk/gov/hmcts/reform/notificationservice/service/NotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/notificationservice/service/NotificationServiceTest.java
@@ -5,10 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.notificationservice.clients.ErrorNotificationClient;
+import uk.gov.hmcts.reform.notificationservice.clients.ErrorNotificationResponse;
+import uk.gov.hmcts.reform.notificationservice.data.Notification;
 import uk.gov.hmcts.reform.notificationservice.data.NotificationRepository;
+import uk.gov.hmcts.reform.notificationservice.data.NotificationStatus;
+import uk.gov.hmcts.reform.notificationservice.model.common.ErrorCode;
 
+import java.time.Instant;
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
@@ -16,11 +27,14 @@ class NotificationServiceTest {
     @Mock
     private NotificationRepository notificationRepository;
 
+    @Mock
+    private ErrorNotificationClient notificationClient;
+
     private NotificationService notificationService;
 
     @BeforeEach
     void setUp() {
-        notificationService = new NotificationService(notificationRepository);
+        notificationService = new NotificationService(notificationRepository, notificationClient);
     }
 
     @Test
@@ -28,5 +42,37 @@ class NotificationServiceTest {
         notificationService.processPendingNotifications();
 
         verify(notificationRepository, times(1)).findPending();
+        verifyNoInteractions(notificationClient);
+    }
+
+    @Test
+    void should_send_error_notification_to_client_and_update_db() {
+        // given
+        var notification = getSampleNotification();
+        var notificationId = "notification ID";
+        given(notificationRepository.findPending()).willReturn(Collections.singletonList(notification));
+        given(notificationClient.notify(any())).willReturn(new ErrorNotificationResponse(notificationId));
+
+        // when
+        notificationService.processPendingNotifications();
+
+        // then
+        verify(notificationRepository, times(1)).markAsSent(notification.id, notificationId);
+    }
+
+    private Notification getSampleNotification() {
+        return new Notification(
+            1L,
+            null,
+            "zip_file_name",
+            "po_box",
+            "service",
+            "DCN",
+            ErrorCode.ERR_METAFILE_INVALID,
+            "invalid metafile",
+            Instant.now(),
+            null,
+            NotificationStatus.PENDING
+        );
     }
 }
