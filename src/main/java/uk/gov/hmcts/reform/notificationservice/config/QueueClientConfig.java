4780386957859d9ac71611f10c410e6a8059e9bd
mdayican <mustafa.dayican@hmcts.net>
2020-03-19 17:05:18 +0000

    fix queue config

--- src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
@@ -6,11 +6,13 @@ import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class QueueClientConfig {
 
     @Bean
-    public IMessageReceiver readNotificationsQueueClient(
+    public IMessageReceiver notificationsMessageReceiver(
         @Value("${queue.notifications.read-connection-string}") String connectionString)
         throws InterruptedException, ServiceBusException {
         return ClientFactory.createMessageReceiverFromConnectionString(
