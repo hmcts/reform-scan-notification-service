62c3a210aaec29d714efe6cf1a9138629cbfdb6e
mdayican <mustafa.dayican@hmcts.net>
2020-03-19 21:38:13 +0000

    fix integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/config/SwaggerPublisher.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockFilterConfig;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @SpringJUnitWebConfig
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("integrationTest")
 class SwaggerPublisher {
 
     @Autowired
--- src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
@@ -7,11 +7,13 @@ import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class QueueClientConfig {
 
     @Bean
+    @Profile("!integrationTest")
     public IMessageReceiver notificationsMessageReceiver(
         @Value("${queue.notifications.read-connection-string}") String connectionString)
         throws InterruptedException, ServiceBusException {
