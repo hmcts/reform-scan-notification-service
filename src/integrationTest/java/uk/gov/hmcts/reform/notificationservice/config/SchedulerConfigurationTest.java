4f74e68a22f2bf211a5f0d54af380e2c1b794f95
Donatas Martinkus <doncem@users.noreply.github.com>
2020-03-23 14:35:03 +0000

    Add placeholder for shedlock feature tests

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/config/SchedulerConfigurationTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.notificationservice.config;
+
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(properties = {
+    "scheduling.task.notifications-consume.enable=true" // task is not there yet, but config is
+})
+public class SchedulerConfigurationTest {
+
+    @SpyBean
+    private LockProvider lockProvider;
+
+    @Test
+    public void should_integrate_with_shedlock() throws Exception {
+        ArgumentCaptor<LockConfiguration> configCaptor = ArgumentCaptor.forClass(LockConfiguration.class);
+
+        // wait for asynchronous run of the scheduled task in background
+        Thread.sleep(2000);
+
+        verify(lockProvider, atLeastOnce()).lock(configCaptor.capture());
+        assertThat(configCaptor.getAllValues())
+            .extracting(LockConfiguration::getName)
+            .isEmpty(); // will not be empty once schedules kick in
+    }
+}
