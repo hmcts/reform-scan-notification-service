e6963e0f464727b3018cd3cbd55534699cd50bcb
apereverzin <andrey.pereverzin@hotmail.com>
2020-03-24 10:17:02 +0000

    Code review

--- src/main/java/uk/gov/hmcts/reform/notificationservice/config/AuthServiceConfig.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/config/AuthServiceConfig.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.notificationservice.config;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import uk.gov.hmcts.reform.authorisation.validators.ServiceAuthTokenValidator;
-
-import java.util.List;
 
 @Configuration
 public class AuthServiceConfig {
@@ -17,26 +14,5 @@ public class AuthServiceConfig {
     @ConditionalOnProperty(name = "idam.s2s-auth.url")
     public AuthTokenValidator tokenValidator(ServiceAuthorisationApi s2sApi) {
         return new ServiceAuthTokenValidator(s2sApi);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "idam.s2s-auth.url", havingValue = "false")
-    public AuthTokenValidator tokenValidatorStub() {
-        return new AuthTokenValidator() {
-            @Override
-            public void validate(String token) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public void validate(String token, List<String> roles) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public String getServiceName(String token) {
-                return "some_service_name";
-            }
-        };
     }
 }
