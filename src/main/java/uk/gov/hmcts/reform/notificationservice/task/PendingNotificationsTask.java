13358f2091310c31da6c13b7b644bd45ffae7c43
donatas M <1420191+doncem@users.noreply.github.com>
2020-03-24 09:48:17 +0000

    Enable pending notifications task (#54)

--- src/main/java/uk/gov/hmcts/reform/notificationservice/task/PendingNotificationsTask.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/task/PendingNotificationsTask.java
@@ -1,16 +1,24 @@
 package uk.gov.hmcts.reform.notificationservice.task;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.notificationservice.service.NotificationService;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
+@ConditionalOnProperty(
+    prefix = "scheduling.task",
+    name = PendingNotificationsTask.TASK_NAME + ".enabled",
+    matchIfMissing = true
+)
 public class PendingNotificationsTask {
 
     private static final Logger log = getLogger(PendingNotificationsTask.class);
-    private static final String TASK_NAME = "pending-notifications";
+    public static final String TASK_NAME = "pending-notifications";
 
     private final NotificationService notificationService;
 
@@ -18,6 +26,8 @@ public class PendingNotificationsTask {
         this.notificationService = notificationService;
     }
 
+    @Scheduled(fixedDelayString = "${scheduling.task.pending-notifications.delay}") // see `values.yaml` in charts
+    @SchedulerLock(name = TASK_NAME) // lock is needed so same notifications won't get through from different nodes
     public void run() {
         log.info("Started {} task", TASK_NAME);
 
