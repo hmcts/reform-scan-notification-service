cb8b075c13da28e185105a16d564165eefb3c28d
apereverzin <andrey.pereverzin@hotmail.com>
2020-03-24 12:00:26 +0000

    Code review

--- src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/config/AuthServiceConfig.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/config/AuthServiceConfig.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.reform.notificationservice.config;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 
-import java.util.List;
+import static org.mockito.Mockito.mock;
 
 @Configuration
 public class AuthServiceConfig {
@@ -14,21 +13,6 @@ public class AuthServiceConfig {
     @Bean
     @ConditionalOnProperty(name = "s2s-auth.url", havingValue = "false")
     public AuthTokenValidator tokenValidator() {
-        return new AuthTokenValidator() {
-            @Override
-            public void validate(String token) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public void validate(String token, List<String> roles) {
-                throw new NotImplementedException();
-            }
-
-            @Override
-            public String getServiceName(String token) {
-                return "some_service_name";
-            }
-        };
+        return mock(AuthTokenValidator.class);
     }
 }
