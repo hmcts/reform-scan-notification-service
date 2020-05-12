270b7c549f5e910b2620f42791810141acdad2a6
mdayican <mustafa.dayican@hmcts.net>
2020-05-12 09:44:53 +0100

    remove logging

--- src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/config/QueueClientConfig.java
@@ -11,35 +11,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
 @Configuration
 public class QueueClientConfig {
 
     private static final Logger log = LoggerFactory.getLogger(QueueClientConfig.class);
-
-    private static String ENTITY_PATH = "EntityPath=";
 
     @Bean
     @ConditionalOnProperty(name = "queue.notifications.read-connection-string")
     public IMessageReceiver notificationsMessageReceiver(
         @Value("${queue.notifications.read-connection-string}") String connectionString)
         throws InterruptedException, ServiceBusException {
-        logQueueName(connectionString);
         return ClientFactory.createMessageReceiverFromConnectionString(
             connectionString,
             ReceiveMode.PEEKLOCK
         );
-    }
-
-    private void logQueueName(String connectionString) {
-        String[] split = connectionString.split(";");
-        Arrays.stream(split)
-            .filter(e -> e.startsWith(ENTITY_PATH))
-            .findFirst()
-            .ifPresent(c ->
-                log.warn("Notification service connected to queue: {} ", c.replace(ENTITY_PATH, ""))
-            );
     }
 
 }
