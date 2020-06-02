c131f39ed61979f49ce00f6ed33889c65bc9652f
Donatas Martinkus <doncem@users.noreply.github.com>
2020-06-02 12:41:04 +0100

    Remove junit4 class usages

--- src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/controller/NotificationControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/controller/NotificationControllerTest.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.notificationservice.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 import uk.gov.hmcts.reform.authorisation.exceptions.ServiceException;
@@ -30,7 +28,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = NotificationController.class)
 public class NotificationControllerTest {
 
