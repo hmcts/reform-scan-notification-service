6b3b90fe3f116b86f61c4ef28d4a51b6af476eae
donatas M <1420191+doncem@users.noreply.github.com>
2020-06-25 10:03:37 +0100

    Migrate swagger specs to v3 (#184)

--- src/main/java/uk/gov/hmcts/reform/notificationservice/model/out/NotificationsResponse.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/model/out/NotificationsResponse.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.notificationservice.model.out;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public class NotificationsResponse {
+
     @JsonProperty("count")
+    @Schema(title = "Notification count", name = "count", description = "Number of notifications found")
     public final int count;
 
     @JsonProperty("notifications")
+    @Schema(title = "List of notifications", name = "notifications", description = "Full list of notifications found")
     public final List<NotificationInfo> notifications;
 
     public NotificationsResponse(
