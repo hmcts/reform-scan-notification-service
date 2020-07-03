80a239f109da8471b76a915838cf56989208e069
Aliveni Choppa <aliveni.ch@gmail.com>
2020-07-03 12:58:32 +0100

    fix the sql error - missing semicolon

--- src/main/resources/db/migration/V009__populate-message-id-values-add-unique-constraint.sql
+++ src/main/resources/db/migration/V009__populate-message-id-values-add-unique-constraint.sql
@@ -1,5 +1,5 @@
 UPDATE notifications SET message_id = CONCAT('legacy_', id)
-  WHERE message_id IS NULL
+  WHERE message_id IS NULL;
 
 ALTER TABLE notifications
   ADD CONSTRAINT notifications_message_id UNIQUE (message_id);
