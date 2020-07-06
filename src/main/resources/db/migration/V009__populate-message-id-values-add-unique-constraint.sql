4823e31aba9ccba3eabf95eaccbde8560b2d6d14
Aliveni Choppa <aliveni.ch@gmail.com>
2020-07-06 10:36:44 +0100

    Make message_id not null after populating the values

--- src/main/resources/db/migration/V009__populate-message-id-values-add-unique-constraint.sql
+++ src/main/resources/db/migration/V009__populate-message-id-values-add-unique-constraint.sql
@@ -2,4 +2,5 @@ UPDATE notifications SET message_id = CONCAT('legacy_', id)
   WHERE message_id IS NULL;
 
 ALTER TABLE notifications
+  ALTER COLUMN message_id SET NOT NULL,
   ADD CONSTRAINT notifications_message_id UNIQUE (message_id);
