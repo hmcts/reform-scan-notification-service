4d377dffdd1cec9901d5af008d5f60f00627ab4e
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-09-29 12:59:38 +0100

    Added db to readiness. Integration test (#258)
    * Added db to readiness. Integration test
    
    * Renamed test method
    
    * Removed readinessState

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/health/ReadinessTest.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.reform.notificationservice.health;
+
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockFilterConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class ReadinessTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        var filter = new WebRequestTrackingFilter();
+        filter.init(new MockFilterConfig());
+        mockMvc = webAppContextSetup(wac).addFilters(filter).build();
+    }
+
+    @Test
+    void readiness_should_contain_db_status() throws Exception {
+        mockMvc
+            .perform(
+                get("/health/readiness")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("UP"))
+            .andExpect(jsonPath("$.components.db.status").value("UP"))
+        ;
+    }
+}
