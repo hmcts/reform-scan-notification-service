e649569a1b63e49410ed7af7a6c1d0c2ab04ab01
MUSTAFA <mustafa.dayican@hmcts.net>
2021-04-22 11:29:41 +0100

    remove EnableCircuitBreaker

--- src/main/java/uk/gov/hmcts/reform/notificationservice/Application.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/Application.java
@@ -2,13 +2,11 @@ package uk.gov.hmcts.reform.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
