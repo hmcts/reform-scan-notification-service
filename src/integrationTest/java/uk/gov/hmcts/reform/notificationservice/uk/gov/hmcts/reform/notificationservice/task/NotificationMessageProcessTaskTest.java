8e1134bf4eb4166d33a2dbef2c63cdb95370e367
mdayican <mustafa.dayican@hmcts.net>
2021-06-16 14:05:04 +0100

    add new test

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/notificationservice/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTaskTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.notificationservice.task;
+
+import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(properties = {"scheduling.task.notifications-consume.enable=true"})
+public class NotificationMessageProcessTaskTest {
+
+    @SpyBean
+    private NotificationMessageProcessTask notificationMessageProcessTask;
+
+    @MockBean
+    private ServiceBusProcessorClient serviceBusProcessorClient;
+
+    @Test
+    public void should_start_ServiceBusProcessorClient() {
+        verify(serviceBusProcessorClient).start();
+    }
+}
