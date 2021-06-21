a15f1d5560abf00038f0416bc251ee2e19fe1f01
MUSTAFA <mustafa.dayican@hmcts.net>
2021-06-21 16:29:27 +0100

    DTSBPS-637 add check for queue listener (#533)

---
+++ src/test/java/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTaskTest.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.reform.notificationservice.task;
+
+import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import io.github.netmikey.logunit.api.LogCapturer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+import static org.slf4j.event.Level.ERROR;
+import static org.slf4j.event.Level.INFO;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationMessageProcessTaskTest {
+
+    @RegisterExtension
+    public LogCapturer logs = LogCapturer.create().captureForType(NotificationMessageProcessTask.class);
+
+    @InjectMocks
+    private NotificationMessageProcessTask notificationMessageProcessTask;
+
+    @Mock
+    private ServiceBusProcessorClient serviceBusProcessorClient;
+
+    @Test
+    public void should_log_when_listener_is_not_working() {
+        given(serviceBusProcessorClient.isRunning()).willReturn(false);
+        notificationMessageProcessTask.checkServiceBusProcessorClient();
+        logs.assertContains(
+            event -> event.getLevel() == ERROR,
+            "Notification queue consume listener is NOT running!!!"
+        );
+    }
+
+    @Test
+    public void should_log_when_listener_is_working() {
+        given(serviceBusProcessorClient.isRunning()).willReturn(true);
+        notificationMessageProcessTask.checkServiceBusProcessorClient();
+        logs.assertContains(
+            event -> event.getLevel() == INFO,
+            "Notification queue consume listener is NOT running!!!"
+        );
+    }
+}
