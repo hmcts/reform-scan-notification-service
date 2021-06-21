c950b17840c02b275fea3e02e2ae4231af9f88c0
mdayican <mustafa.dayican@hmcts.net>
2021-06-21 14:45:49 +0100

    add check for queue listener

--- src/main/java/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTask.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTask.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.reform.notificationservice.task;
 
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +14,7 @@ import javax.annotation.PostConstruct;
 public class NotificationMessageProcessTask {
 
     private final ServiceBusProcessorClient serviceBusProcessorClient;
+    private static final Logger log = LoggerFactory.getLogger(NotificationMessageProcessTask.class);
 
     public NotificationMessageProcessTask(
         ServiceBusProcessorClient serviceBusProcessorClient
@@ -21,5 +25,14 @@ public class NotificationMessageProcessTask {
     @PostConstruct
     void startProcessor() {
         serviceBusProcessorClient.start();
+    }
+
+    @Scheduled(fixedDelayString = "${scheduling.task.notifications-check.delay}")
+    public void checkServiceBusProcessorClient() {
+        if (!serviceBusProcessorClient.isRunning()) {
+            log.error("Notification queue consume listener is NOT running!!!");
+        } else {
+            log.info("Notification queue consume listener is working.");
+        }
     }
 }
