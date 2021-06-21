b57b522955ffab541edd9281924b7a3ca42a6361
mdayican <mustafa.dayican@hmcts.net>
2021-06-21 14:47:19 +0100

    Revert "add check for queue listener"
    This reverts commit c950b17840c02b275fea3e02e2ae4231af9f88c0.

--- src/main/java/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTask.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/task/NotificationMessageProcessTask.java
@@ -1,10 +1,7 @@
 package uk.gov.hmcts.reform.notificationservice.task;
 
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,7 +11,6 @@ import javax.annotation.PostConstruct;
 public class NotificationMessageProcessTask {
 
     private final ServiceBusProcessorClient serviceBusProcessorClient;
-    private static final Logger log = LoggerFactory.getLogger(NotificationMessageProcessTask.class);
 
     public NotificationMessageProcessTask(
         ServiceBusProcessorClient serviceBusProcessorClient
@@ -25,14 +21,5 @@ public class NotificationMessageProcessTask {
     @PostConstruct
     void startProcessor() {
         serviceBusProcessorClient.start();
-    }
-
-    @Scheduled(fixedDelayString = "${scheduling.task.notifications-check.delay}")
-    public void checkServiceBusProcessorClient() {
-        if (!serviceBusProcessorClient.isRunning()) {
-            log.error("Notification queue consume listener is NOT running!!!");
-        } else {
-            log.info("Notification queue consume listener is working.");
-        }
     }
 }
