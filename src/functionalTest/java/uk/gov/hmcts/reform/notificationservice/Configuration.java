f3b3ad0e3490fe7fdf9c3d989e294cf5251e0da7
amstevenson <addstevenson@hotmail.com>
2023-05-24 10:53:52 +0100

    removing suppressions

--- src/functionalTest/java/uk/gov/hmcts/reform/notificationservice/Configuration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/notificationservice/Configuration.java
@@ -4,12 +4,9 @@ import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 final class Configuration {
 
-    private static final Logger log = LoggerFactory.getLogger(Configuration.class);
     private static final Config CONFIG = ConfigFactory.load();
 
     private static final String ERROR_MESSAGE = "Failure building queue client";
@@ -38,8 +35,8 @@ final class Configuration {
             NOTIFICATION_QUEUE_ACCESS_KEY_WRITE
         );
 
-        log.info("Connecting with namespace: {} to queue {}",
-                 NOTIFICATION_QUEUE_NAMESPACE, NOTIFICATION_QUEUE_NAME);
+        System.out.printf("Connecting with namespace: %s to queue %s",
+                          NOTIFICATION_QUEUE_NAMESPACE, NOTIFICATION_QUEUE_NAME);
 
         return new ServiceBusClientBuilder()
             .connectionString(connectionString)
