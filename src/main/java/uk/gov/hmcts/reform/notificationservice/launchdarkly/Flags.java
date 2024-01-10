8d05e052314a90b48ac3242fc640d7d75a6018ca
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-01-10 13:29:10 +0000

    Added main launch darkly code

---
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/launchdarkly/Flags.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.notificationservice.launchdarkly;
+
+public final class Flags {
+    private Flags() {
+
+    }
+
+    public static final String REFORM_SCAN_NOTIFICATION_SERVICE_TEST = "reform-scan-notification-service-test";
+}
