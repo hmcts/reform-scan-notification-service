2dffd3dabdad51e5177203a47d575759de474a9e
Ruth Bovell <ruthb111@outlook.com>
2024-10-08 17:31:19 +0100

    - removes unneeded methods

--- src/main/java/uk/gov/hmcts/reform/notificationservice/util/NotificationConverter.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/util/NotificationConverter.java
@@ -1,15 +1,10 @@
 package uk.gov.hmcts.reform.notificationservice.util;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.reform.notificationservice.data.NewNotification;
 import uk.gov.hmcts.reform.notificationservice.data.Notification;
-import uk.gov.hmcts.reform.notificationservice.data.NotificationEntity;
-import uk.gov.hmcts.reform.notificationservice.model.in.NotificationMsg;
 import uk.gov.hmcts.reform.notificationservice.model.in.NotificationMsgRequest;
 import uk.gov.hmcts.reform.notificationservice.model.out.NotificationInfo;
-
-import java.time.Instant;
 
 import static java.lang.Math.min;
 
@@ -53,46 +48,6 @@ public final class NotificationConverter {
             notificationMsg.errorDescription,
             null,
             client
-        );
-    }
-
-    public static NotificationEntity toNotificationEntity(NotificationMsgRequest notificationMsg) {
-        NotificationEntity notificationEntity = new NotificationEntity();
-        notificationEntity.setZipFileName(notificationEntity.getZipFileName());
-        notificationEntity.setPoBox(notificationMsg.getPoBox());
-        notificationEntity.setContainer(notificationMsg.getContainer());
-        notificationEntity.setService(notificationMsg.getService());
-        notificationEntity.setDocumentControlNumber(notificationMsg.getDocumentControlNumber());
-        notificationEntity.setErrorCode(notificationMsg.getErrorCode());
-        notificationEntity.setErrorDescription(notificationMsg.getErrorDescription());
-        notificationEntity.setCreatedAt(Instant.now());
-        notificationEntity.setCreatedAt(Instant.now());
-        notificationEntity.setMessageId(RandomStringUtils.random(10));
-        notificationEntity.setClient("primary");
-        return notificationEntity;
-    }
-
-    public static NotificationInfo toNotificationResponse(NotificationEntity notification) {
-        return new NotificationInfo(
-            notification.getId(),
-            notification.getConfirmationId(),
-            notification.getZipFileName(),
-            notification.getPoBox(),
-            notification.getContainer(),
-            notification.getService(),
-            notification.getDocumentControlNumber(),
-            notification.getErrorCode().name(),
-            notification.getErrorDescription() == null
-                ?
-                ""
-                :
-                notification.getErrorDescription().substring(
-                    0,
-                    min(MAX_ERROR_DESCRIPTION_LENGTH, notification.getErrorDescription().length())
-                ),
-            notification.getCreatedAt(),
-            notification.getProcessedAt(),
-            notification.getStatus().name()
         );
     }
  }
