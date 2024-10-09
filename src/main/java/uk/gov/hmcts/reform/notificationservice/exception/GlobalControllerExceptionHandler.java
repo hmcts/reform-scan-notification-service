1dcdba99b1c6baccb8464fbd404e0b4807acf850
Ruth Bovell <ruthb111@outlook.com>
2024-10-09 14:20:44 +0100

    - merge the exception handlers together - removes unneeded bad request exception

--- src/main/java/uk/gov/hmcts/reform/notificationservice/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/notificationservice/exception/GlobalControllerExceptionHandler.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -42,7 +41,7 @@ public class GlobalControllerExceptionHandler {
 
     /**
      * Handles NotFoundException.
-     *
+     * Puts the exception message into the response body and returns a status code of 404 (NOT FOUND)
      * @param ex the exception
      * @return the response entity
      * @throws JsonProcessingException if the response entity cannot be converted to a string
@@ -56,26 +55,47 @@ public class GlobalControllerExceptionHandler {
         return new ResponseEntity<>(new ObjectMapper().writeValueAsString(error), responseHeaders, HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Handles the FailedDependency exception.
+     * Puts the notification info associated with the exception into the response body and returns a status code of 424 (FAILED DEPENDENCY)
+     * @param ex the exception
+     * @return the response entity
+     * @throws JsonProcessingException
+     */
     @ExceptionHandler(FailedDependencyException.class)
     ResponseEntity<NotificationInfo> failedDependencyExceptionHandler(final FailedDependencyException ex) throws JsonProcessingException {
-        HashMap<String, String> error = new HashMap<>();
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(CONTENT_TYPE, APPLICATION_JSON);
-        error.put(MESSAGE, ex.getMessage());
+        log.error(ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getNotificationInfo(), responseHeaders, HttpStatus.FAILED_DEPENDENCY);
     }
 
+    /**
+     * Handles the MethodArgumentNotValid exception.
+     * When the body of the request has correct syntax but the values fail validation the MethodArgumentNotValidException
+     * is thrown. This method takes the fields that failed validation and the reasons why and puts them in the response body
+     * with a status code of 400 (BAD REQUEST)
+     * @param ex the exception
+     * @return the response entity
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handle(MethodArgumentNotValidException ex) {
         Map<String, String> errorMap = new ConcurrentHashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errorMap.put(error.getField(),
                                                                              error.getDefaultMessage()));
-
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap);
     }
 
+    /**
+     * Handles the NumberFormat exception.
+     * Puts the malformed number and a message about the number being invalid into the response body with a status code of
+     * 400 (BAD REQUEST)
+     * @param ex the exception
+     * @return the response entity
+     * @throws JsonProcessingException
+     */
     @ExceptionHandler(NumberFormatException.class)
-    ResponseEntity<String> notFoundExceptionHandler(final NumberFormatException ex, HttpServletRequest request) throws JsonProcessingException {
+    ResponseEntity<String> notFoundExceptionHandler(final NumberFormatException ex) throws JsonProcessingException {
         HashMap<String, String> error = new HashMap<>();
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(CONTENT_TYPE, APPLICATION_JSON);
@@ -84,18 +104,39 @@ public class GlobalControllerExceptionHandler {
         return new ResponseEntity<>(new ObjectMapper().writeValueAsString(error), responseHeaders, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Handles the UnauthenticatedException.
+     * UnauthenticatedException is thrown when authentication is not present, This method handles
+     * the exception by returning a status code of 401 (UNAUTHORIZED)
+     * @param ex the exception
+     * @return the response entity
+     */
     @ExceptionHandler(UnauthenticatedException.class)
     protected ResponseEntity<Void> handleUnauthenticatedException(UnauthenticatedException ex) {
         log.error(ex.getMessage(), ex);
         return status(HttpStatus.UNAUTHORIZED).build();
     }
 
+    /**
+     * Handles the InvalidTokenException.
+     * When the authentication library determines that a token is not valid the InvalidTokenException is thrown.
+     * This method handles the exception by returning a status code of 401 (UNAUTHORIZED)
+     * @param ex the exception
+     * @return the response entity
+     */
     @ExceptionHandler(InvalidTokenException.class)
     protected ResponseEntity<Void> handleInvalidTokenException(InvalidTokenException ex) {
         log.error(ex.getMessage(), ex);
         return status(HttpStatus.UNAUTHORIZED).build();
     }
 
+    /**
+     * Handles the ServiceException.
+     * When the authentication library encounters a non-Feign exception trying to authenticate a token the ServiceException is thrown
+     * This method handles the exception by returning a status code of 500 (INTERNAL_SERVER_ERROR)
+     * @param ex the exception
+     * @return the response entity
+     */
     @ExceptionHandler(ServiceException.class)
     protected ResponseEntity<Void> handleServiceException(ServiceException ex) {
         log.error(ex.getMessage(), ex);
